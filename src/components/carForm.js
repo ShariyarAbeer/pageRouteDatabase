8569e427719ab2a500961350610e49357fe9e8dc
EyePatch <007abeershariyar@gmail.com>
2020-07-30 04:39:51 +0600

    post https

--- src/components/carForm.js
+++ src/components/carForm.js
@@ -1,41 +1,71 @@
 import React, { Component } from "react";
 import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
 
 class CarForm extends Component {
-  state = {};
-  render() {
-    return (
-      <form>
-        <div class="form-group">
-          <input
-            type="text"
-            class="form-control"
-            id="exampleInputEmail1"
-            placeholder="Car Model and Name"
-          ></input>
-        </div>
-        <div class="form-group">
-          <input
-            type="text"
-            class="form-control"
-            id="exampleInputEmail1"
-            placeholder="Car ID Number"
-          ></input>
-        </div>
-        <div class="form-group">
-          <input
-            type="text"
-            class="form-control"
-            id="exampleInputEmail1"
-            placeholder="Owner Name"
-          ></input>
-        </div>
-        <button type="submit" class="btn btn-primary">
-          Submit
-        </button>
-      </form>
-    );
-  }
+	constructor() {
+		super();
+		this.state = {
+			postData: "",
+			postResult: "",
+		};
+	}
+
+	onChangeHandler = (event) => {
+		var myData = event.target.value;
+		this.setState({ postData: myData });
+	};
+
+	onClickHandler = () => {
+		axios
+			.post("", this.state.postData)
+			.then((response) => {
+				this.setState({ postResult: response.data });
+				alert(this.state.postResult);
+			})
+			.catch((error) => {
+				alert("Something went wrong");
+			});
+	};
+
+	render() {
+		return (
+			// <form>
+			//   <div class="form-group">
+			//     <input
+			//       type="text"
+			//       class="form-control"
+			//       id="exampleInputEmail1"
+			//       placeholder="Car Model and Name"
+			//     ></input>
+			//   </div>
+			//   <div class="form-group">
+			//     <input
+			//       type="text"
+			//       class="form-control"
+			//       id="exampleInputEmail1"
+			//       placeholder="Car ID Number"
+			//     ></input>
+			//   </div>
+			//   <div class="form-group">
+			//     <input
+			//       type="text"
+			//       class="form-control"
+			//       id="exampleInputEmail1"
+			//       placeholder="Owner Name"
+			//     ></input>
+			//   </div>
+			//   <button type="submit" class="btn btn-primary">
+			//     Submit
+			//   </button>
+			// </form>
+
+			<div>
+				<input onChange={this.onChangeHandler} type="text" />
+				<button onClick={this.onClickHandler}>Send</button>
+			</div>
+		);
+	}
 }
 
 export default CarForm;
